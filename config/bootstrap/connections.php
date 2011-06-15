a618c48e474b8d1ae2f1e97a39cfc01f956e9992
Martin Samson <pyrolian@gmail.com>
2011-06-14 22:45:59 -0400

    Added sqlite db and gitignore

--- config/bootstrap/connections.php
+++ config/bootstrap/connections.php
@@ -37,35 +37,10 @@
  */
 use lithium\data\Connections;
 
-/**
- * Uncomment this configuration to use MongoDB as your default database.
- */
-// Connections::add('default', array(
-// 	'type' => 'MongoDb',
-// 	'host' => 'localhost',
-// 	'database' => 'my_app'
-// ));
-
-/**
- * Uncomment this configuration to use CouchDB as your default database.
- */
-// Connections::add('default', array(
-// 	'type' => 'http',
-// 	'adapter' => 'CouchDb',
-// 	'host' => 'localhost',
-// 	'database' => 'my_app'
-// ));
-
-/**
- * Uncomment this configuration to use MySQL as your default database.
- */
-// Connections::add('default', array(
-// 	'type' => 'database',
-// 	'adapter' => 'MySql',
-// 	'host' => 'localhost',
-// 	'login' => 'root',
-// 	'password' => '',
-// 	'database' => 'my_app'
-// ));
+Connections::add('default', array(
+ 	'type' => 'database',
+ 	'adapter' => 'Sqlite3',
+ 	'database' => LITHIUM_APP_PATH . '/resources/db/sqlite.db'
+));
 
 ?>
